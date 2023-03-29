9f9ae9dc3c547bd1cd771baffaa1e63c5ac7ca20
forofonof <DanielResult1@outlook.com>
2023-03-29 13:14:22 +0300

    FIX_1.1

--- Homework_52/Program.cs
+++ Homework_52/Program.cs
@@ -29,7 +29,7 @@ class Menu
     {
         const string SortFullName = "1";
         const string SortAge = "2";
-        const string SearchCaseReport = "3";
+        const string SearchDisease = "3";
 
         bool isWork = true;
 
@@ -37,9 +37,9 @@ class Menu
 
         while (isWork)
         {
-            Console.WriteLine($"{SortFullName} - Отсортировать всех больных по ФИО.");
+            Console.WriteLine($"{SortFullName} - Отсортировать всех больных в алфавитном порядке");
             Console.WriteLine($"{SortAge} - Отсортировать всех больных по возрасту.");
-            Console.WriteLine($"{SearchCaseReport} - Вывести больных с определенным заболеванием.");
+            Console.WriteLine($"{SearchDisease} - Вывести больных с определенным заболеванием.");
 
             string userInput = Console.ReadLine();
 
@@ -53,7 +53,7 @@ class Menu
                     _hospital.ShowPatientsByAge();
                     break;
 
-                case SearchCaseReport:
+                case SearchDisease:
                     _hospital.ShowPatientsByDisease();
                     break;
 
@@ -76,29 +76,16 @@ class Hospital
 
     public void ShowPatientsByFullName()
     {
-        Console.WriteLine("Введите Фамилию и Имя:");
-        string userInput = Console.ReadLine();
-
-        var filteredPatients = _database.GetPatients().Where(patient => patient.FullName.Equals(userInput)).ToList();
+        var filteredPatients = _database.GetPatients().OrderBy(patient => patient.FullName).ToList();
 
         ShowPatients(filteredPatients);
     }
 
     public void ShowPatientsByAge()
     {
-        Console.WriteLine("Введите возраст:");
-        string userInput = Console.ReadLine();
+        var filteredPatients = _database.GetPatients().OrderBy(patient => patient.Age).ToList();
 
-        if (int.TryParse(userInput, out int age))
-        {
-            var filteredPatients = _database.GetPatients().Where(patient => patient.Age == age).OrderBy(patient => patient.Age).ToList();
-
-            ShowPatients(filteredPatients);
-        }
-        else
-        {
-            Console.WriteLine("Ошибка! Введите число.");
-        }
+        ShowPatients(filteredPatients);
     }
 
     public void ShowPatientsByDisease()
@@ -140,6 +127,7 @@ class Database
     public List<Patient> AddPatientsInList()
     {
         _patients.AddRange(_patientFactory.CreateMultiplePatients());
+
         return _patients;
     }
 
@@ -151,19 +139,21 @@ class Database
 
 class PatientFactory
 {
-    private readonly Random _random = new Random();
     private readonly string[] _diseases = { "Сахарный диабет", "Простуда", "Корона", "Мигрень", "Грипп", "Гастрит" };
 
     private readonly string[] _fullNames = 
     {
-        "Леонова Екатерина", "Соколов Артём", "Гусева Амина", "Соколова Ева", "Кузнецов Кирилл", "Иванова Марина",
+        "Артышкина Екатерина", "Соколов Артём", "Гусева Амина", "Соколова Ева", "Кузнецов Кирилл", "Иванова Марина",
         "Смирнов Иван", "Форофонов Вячеслав", "Ларионова Елизавета", "Родин Марк", "Балашова Ксения", "Фролова Елизавета"
     };
 
+    private readonly Random _random = new Random();
+
     public List<Patient> CreateMultiplePatients()
     {
+        int numberOfPatients = 15;
+
         List<Patient> patients = new List<Patient>();
-        int numberOfPatients = 25;
 
         for (int i = 0; i < numberOfPatients; i++)
         {
@@ -179,8 +169,8 @@ class PatientFactory
         int ageMinimum = 18;
         int ageMaximum = 70;
 
-        string name = _fullNames[_random.Next(_fullNames.Length)];
         int age = _random.Next(ageMinimum, ageMaximum);
+        string name = _fullNames[_random.Next(_fullNames.Length)];
         string disease = _diseases[_random.Next(_diseases.Length)];
 
         return new Patient(name, age, disease);
